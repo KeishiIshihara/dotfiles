07c23ad8a8705ffdcce1f4afbc532aae69d8c720
KeishiIshihara <ishihara@aisl.cs.tut.ac.jp>
2019-03-15 23:43:08 +0900

    disabled

--- dotFilesLink.sh
+++ dotFilesLink.sh
@@ -4,26 +4,26 @@
 ############################
 ##### Create symlinks ######
 ############################
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.ros_setup ~/.ros_setup
+# ln -s ~/dotfiles/.bashrc ~/.bashrc
+# # ln -s ~/dotfiles/.ros_setup ~/.ros_setup
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
-#ln -s ~/dotfiles/.ycm_extra_conf.py ~/catkin_ws/.ycm_extra_conf.py
+# # ln -s ~/dotfiles/.vimrc ~/.vimrc
+# # ln -s ~/dotfiles/.vim ~/.vim
+# # ln -s ~/dotfiles/.ycm_extra_conf.py ~/catkin_ws/.ycm_extra_conf.py
 
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.tmux-powerlinerc ~/.tmux-powerlinerc
-ln -s ~/dotfiles/.tmuxinator ~/.tmuxinator
-#ln -s ~/dotfiles/.tmuxautorun ~/.tmuxautorun
+# ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+# ln -s ~/dotfiles/.tmux-powerlinerc ~/.tmux-powerlinerc
+# ln -s ~/dotfiles/.tmuxinator ~/.tmuxinator
+# #ln -s ~/dotfiles/.tmuxautorun ~/.tmuxautorun
 
-ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
+# ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
 
 
 
 ####################################
 ##### Install Shougo/neobundle #####
 ####################################
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+# git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 # Then, run :NeoBundleInstall on vim to install the other plugins
 
 
@@ -32,54 +32,54 @@ git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 ##### Install tmux-powerline #####
 ##################################
 # Install tmux
-sudo apt-get install aptitude
-sudo aptitude install tmux # NOTE: tmux should be version 1.8. Please check whether you have the right version. If not, downgrade it via apt.
+# sudo apt-get install aptitude
+# sudo aptitude install tmux # NOTE: tmux should be version 1.8. Please check whether you have the right version. If not, downgrade it via apt.
 
-sudo gem install rubygems-update
-sudo update_rubygems
-sudo gem install tmuxinator
+# sudo gem install rubygems-update
+# sudo update_rubygems
+# sudo gem install tmuxinator
 
-git clone git://github.com/erikw/tmux-powerline.git ~/tmux-powerline
+# git clone git://github.com/erikw/tmux-powerline.git ~/tmux-powerline
 
 
 
 #########################################################################
 ##### Install Ricty font (http://www.rs.tus.ac.jp/yyusa/ricty.html) #####
 #########################################################################
-# Install FontForge
-sudo add-apt-repository ppa:fontforge/fontforge
-sudo apt-get update
-sudo apt-get install fontforge
+# # Install FontForge
+# sudo add-apt-repository ppa:fontforge/fontforge
+# sudo apt-get update
+# sudo apt-get install fontforge
 
-# Get Google Fonts Inconsolata and M+ IPA synthesized font Migu 1M
-mkdir ~/.fonts
-cp Ricty/Inconsolata/Inconsolata-Bold.ttf ~/.fonts/
-cp Ricty/Inconsolata/Inconsolata-Regular.ttf ~/.fonts/
-cp Ricty/migu-1m-20150712/migu-1m-bold.ttf ~/.fonts/
-cp Ricty/migu-1m-20150712/migu-1m-regular.ttf ~/.fonts/
+# # Get Google Fonts Inconsolata and M+ IPA synthesized font Migu 1M
+# mkdir ~/.fonts
+# cp Ricty/Inconsolata/Inconsolata-Bold.ttf ~/.fonts/
+# cp Ricty/Inconsolata/Inconsolata-Regular.ttf ~/.fonts/
+# cp Ricty/migu-1m-20150712/migu-1m-bold.ttf ~/.fonts/
+# cp Ricty/migu-1m-20150712/migu-1m-regular.ttf ~/.fonts/
 
-# Run Ricty ge
-./Ricty/ricty_generator.sh auto
-# or ./Ricty/ricty_generator.sh Inconsolata-{Regular,Bold}.ttf migu-1m-{regular,bold}.ttf
-mv Ricty-* ~/.fonts/
-mv RictyDiscord-* ~/.fonts/
+# # Run Ricty ge
+# ./Ricty/ricty_generator.sh auto
+# # or ./Ricty/ricty_generator.sh Inconsolata-{Regular,Bold}.ttf migu-1m-{regular,bold}.ttf
+# mv Ricty-* ~/.fonts/
+# mv RictyDiscord-* ~/.fonts/
 
-# Scan font directories
-sudo fc-cache -vf
-fc-list | grep Ri
+# # Scan font directories
+# sudo fc-cache -vf
+# fc-list | grep Ri
 
-# Set Ricty as default font
-gconftool-2 --get /apps/gnome-terminal/profiles/Default/font # Show current font
-echo "->"
-gconftool-2 --set --type string /apps/gnome-terminal/profiles/Default/font "Ricty Regular 12"
+# # Set Ricty as default font
+# gconftool-2 --get /apps/gnome-terminal/profiles/Default/font # Show current font
+# echo "->"
+# gconftool-2 --set --type string /apps/gnome-terminal/profiles/Default/font "Ricty Regular 12"
 
 
 
 #########################################################################
 ##### Install tex-related software #####
 #########################################################################
-sudo apt-get install texlive-full
-sudo apt-get install latexmk
+# sudo apt-get install texlive-full
+# sudo apt-get install latexmk
 
 
 
