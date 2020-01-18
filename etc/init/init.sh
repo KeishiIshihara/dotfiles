a7b9676be2996f64c1df27d3ba6c791f3b7bbc5c
KeishiIshihara <ishihara@aisl.cs.tut.ac.jp>
2020-01-18 16:41:21 +0200

    Upgrade init.sh: revised init_for_macOS

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -1,4 +1,103 @@
+#!/bin/bash
+#-----------------------------
+#
+#    Initialize Environment
+#
+#-----------------------------
+
+# TODO;
+# - [ ] Revise init_for_ubuntu
+# - [ ] Revise init_for_mac
+
+
+is_exists() {
+    which "$1" >/dev/null 2>&1
+    return $?
+}
+
+lower() {
+    if [ $# -eq 0 ]; then
+        cat <&0
+    elif [ $# -eq 1 ]; then
+        if [ -f "$1" -a -r "$1" ]; then
+            cat "$1"
+        else
+            echo "$1"
+        fi
+    else
+        return 1
+    fi | tr "[:upper:]" "[:lower:]"
+}
+
+ostype() {
+    # shellcheck disable=SC2119
+    uname | lower
+}
+
+os_detect() {
+    export PLATFORM
+    case "$(ostype)" in
+        *'linux'*)  PLATFORM='linux'   ;;
+        *'darwin'*) PLATFORM='osx'     ;;
+        *'bsd'*)    PLATFORM='bsd'     ;;
+        *)          PLATFORM='unknown' ;;
+    esac
+}
+
+user_detect() {
+    if [ "$(whoami)" = 'ishiharakeishi' ]; then
+        # export PLATFORM
+        CURRENT_USER='ishihara-mac'
+    elif [ "$(whoami)" = 'keishish' ]; then
+        CURRENT_USER='dluef-kde'
+    elif [ "$(whoami)" = 'aisl' ]; then
+        CURRENT_USER='aisl-pc'
+    else
+        CURRENT_USER='unknown'
+    fi
+}
+
+e_newline() {
+    printf "\n" 
+}
+
+e_header() {
+    printf " \033[37;1m%s\033[m\n" "$*" 
+}
+
+e_error() {
+    printf " \033[31m%s\033[m\n" "✖ $*" 1>&2 
+}
+
+e_warning() {
+    printf " \033[31m%s\033[m\n" "$*" 
+}
+
+e_done() {
+    printf " \033[37;1m%s\033[m...\033[32mOK\033[m\n" "✔ $*" 
+}
+
+e_arrow() {
+    printf " \033[37;1m%s\033[m\n" "➜ $*" 
+}
+
+e_success() {
+    printf " \033[37;1m%s\033[m%s...\033[32mOK\033[m\n" "✔ " "$*"
+}
+
+set_dotpath() {
+    if [ -z "${DOTPATH:-}" ]; then
+        DOTPATH=$HOME/dotfiles; export DOTPATH
+        echo " DOTPATH is set as $HOME/dotfiles"
+    else
+        echo " DOTPATH already exists."
+    fi
+}
+
+
 init_for_ubuntu () {
+    set_dotpath
+
     sudo apt-get update
     sudo apt update
     ##############################
@@ -81,55 +180,123 @@ init_for_ubuntu () {
 
 }
 
-init_for_mac () {
+init_for_macOS () {
+    ## make sure that dotpath exits
+    set_dotpath
+
+    ##############################
+    ##### Install Homebrew   #####
+    ##############################
+    e_newline
+    e_header "Installing homebrew..."
+
+    if ! is_exists "brew" ; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        echo 'brew already exists.'
+    fi
+
     brew update
+
     ##############################
-    ##### Install some tools #####
+    ##### Install packages   #####
     ##############################
-    brew install less net-tools curl git make htop
+    e_newline
+    e_header "Installing packages..."
+
+    brew install less 
+    brew install curl 
+    brew install htop 
+    brew install wget 
+    brew install reattach-to-user-namespace 
+    brew install tmux 
+    brew install vim
+    # brew install texlive-full
+    # brew install latexmk
 
     ####################################
     ##### Install Shougo/neobundle #####
     ####################################
-    brew install vim
-    git clone https://github.com/Shougo/neobundle.vim $DOTPATH/.vim/bundle/neobundle.vim
+    # git clone https://github.com/Shougo/neobundle.vim $DOTPATH/.vim/bundle/neobundle.vim
     # Then, run :NeoBundleInstall on vim to install the other plugins
 
     ##################################
     ##### Install tmux-powerline #####
     ##################################
-    # Install tmux
-    # brew install tmux 
-    sudo gem install -y tmuxinator
-    git clone git://github.com/erikw/tmux-powerline.git $DOTPATH/.tmux-powerline
-    git clone https://github.com/powerline/fonts.git
+    gem install tmuxinator
+    if [ ! -d "$DOTPATH/.tmux-powerline" ]; then
+        git clone git://github.com/erikw/tmux-powerline.git $DOTPATH/.tmux-powerline
+    else
+        echo ' ~/.tmux-powerline already exists.'
+    fi
+
+    if [ ! -d "$DOTPATH/fonts" ]; then
+        git clone https://github.com/powerline/fonts.git $DOTPATH/
+    else
+        echo ' ~/fonts already exists.'
+    fi
+
     bash $DOTPATH/fonts/install.sh
 
     #########################################################################
     ##### Install tex-related software #####
     #########################################################################
-    # sudo apt-get install texlive-full
-    # sudo apt-get install latexmk
-    # brew install texlive-full
-    # brew install latexmk
 
     ##################################
     #####     Install pyenv      #####
     ##################################
-    sudo apt update
-    sudo apt install -y zlib1g-dev libssl-dev
-    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-    source $DOTPATH/.bashrc
-
+    e_newline
+    e_done "Initialization"
 }
 
-if [ "$(uname)" = 'Darwin' ]; then
-    echo Hello Mac!
-    init_for_mac
-else
-    echo Hello Ubuntu! You are supposed to be Ubuntu
+
+os_detect
+user_detect
+
+# e_warning '[WARNING] test'
+# e_header '[HEADER] test'
+# e_error '[ERROR] test'
+# e_done '[DONE] test'
+# e_arrow '[ARROW] test'
+# e_success '[ARROW] test'
+# exit 0
+
+
+## Following are my personal used PCs
+if [ "$PLATFORM" = 'osx' ] && [ "$CURRENT_USER" = 'ishihara-mac' ]; then
+    echo 'Initializing for ishihara macOS..'
+    init_for_macOS
+    IS_DONE=1
+
+elif [ "$PLATFORM" = 'linux' ] && [ "$CURRENT_USER" = 'dluef-kde' ]; then
+    echo 'Initializing for KDE 17. ..user edition..'
     init_for_ubuntu
+    IS_DONE=1
+
+elif [ "$PLATFORM" = 'linux' ] && [ "$CURRENT_USER" = 'aisl-pc' ]; then
+    echo 'Initializing for Ubuntu 18.04 in aisl..'
+    init_for_ubuntu
+    IS_DONE=1
+
+else
+    e_warning '[WARNING] This platform is not familiar.'
 fi
 
 
+## This is for new env
+if [ ! "${IS_DONE:=0}" = 1 ]; then
+    e_newline
+    e_header 'Welcome to new PC!'
 
+    if [ "$PLATFORM" = 'osx' ]; then
+        echo 'Initializing for macOS..'
+        init_for_macOS
+
+    elif [ "$PLATFORM" = 'linux' ]; then
+        echo 'Initializing for linux ..'
+        init_for_ubuntu
+
+    else
+        e_error '[ERROR] This platform is not supported.'
+    fi
+fi
